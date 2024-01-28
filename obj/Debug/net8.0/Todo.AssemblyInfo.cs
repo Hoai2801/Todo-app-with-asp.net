e7a64d8a2e40862b34c4121a1d98486a0839c8e0
Hoai2801 <dreamhoaihack@gmail.com>
2024-01-28 21:41:06 +0700

    reformat repo file

--- obj/Debug/net8.0/Todo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Todo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58e5031bd5ba64ee156a1819e6485a00333ff218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bcfeaf602ff1d1df0d4c3eabf345be2a09aa00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
